23088dbfe524daeb1d6e49c6268f667e8ccbabc8
Will Furnass <will@thearete.co.uk>
2016-10-13 10:46:23 +0100

    AdmixTools (for Iceberg): make small fixes to install script and modulefile

--- apps/AdmixTools/4.1/sheffield/iceberg/install_admixtools_4.1.sh
+++ apps/AdmixTools/4.1/sheffield/iceberg/install_admixtools_4.1.sh
@@ -10,20 +10,19 @@ start_dir="$(pwd)"
 scratch_dir="/scratch/${USER}/"
 build_dir="${scratch_dir}/AdmixTools"
 inst_dir="/usr/local/packages6/apps/intel/${compiler_short_version}/AdmixTools/${admixtools_version}"
-env_mod_dir="/usr/local/modulefiles/apps/AdmixTools/${admixtools_version}/intel/"
+env_mod_dir="/usr/local/modulefiles/apps/intel/${compiler_short_version}/AdmixTools/"
 
 # Create and set permissions on installation dir and environment module dir
 # (sudo only needed here)
 for d in "${inst_dir}" "${env_mod_dir}"; do
-    sudo mkdir -p $d
-    sudo chgrp app-admins $d
-    sudo chmod g+x $d
+    mkdir -m 2775 -p $d
+    chgrp app-admins $d
+    chmod g+x $d
 done
 
 # Enable the use of the Intel compiler and MKL (which provides MKL versions of BLAS and LAPACK)
 module load compilers/intel/${compiler_version}
 
-# Ensure the 
 export CPATH=${build_dir}/include:${CPATH}
 export LD_LIBRARY_PATH=${build_dir}/lib:${LD_LIBRARY_PATH}
 
@@ -35,7 +34,7 @@ export LD_LIBRARY_PATH=${build_dir}/lib:${LD_LIBRARY_PATH}
 pushd ${build_dir}
 git checkout def3c5d75d1b10fd3270631f5c64adbf3af04d4d
 
-# Install dependency: GNU Scientific library(gsl)
+# Install dependency: GNU Scientific library (GSL)
 [[ -d gsl-${gsl_version} ]] || curl http://ftp.heanet.ie/mirrors/gnu/gsl/gsl-${gsl_version}.tar.gz | tar -zx
 pushd gsl-${gsl_version}
 ./configure --prefix=$(pwd)/../ || echo 'Could not configure GSL prior to compilation' 1>&2 && exit -1
@@ -62,7 +61,8 @@ popd
 # Move everything in the build dir to the installation dir
 mv * .git .gitignore ${inst_dir}
 popd
+chgrp -R app-admins ${inst_dir}
+chmod -R g+x ${inst_dir}
 
 # Install the Environment Module
-cp "${start_dir}/admixtools_env_mod_${admixtools_version}" "${env_mod_dir}/${compiler_short_version}"
-
+cp "${start_dir}/admixtools_env_mod_${admixtools_version}" "${env_mod_dir}/${admixtools_version}"
